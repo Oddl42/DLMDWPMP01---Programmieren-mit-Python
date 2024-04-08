7fae91131dcdd8d30d4804a90253c07ef12241c8
Willkens <tim.willkens@voith.com>
2024-04-08 15:55:49 +0200

    Modify Classes: Add class TestData () Add class IdealFunctions()
    Modify Bibs:
    Add new libaries

---
+++ Code/Bibs.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  2 07:27:48 2024
+
+@author: WillkeT
+
+import necessary libs
+"""
+
+import numpy as np
+import pandas as pd
+from bokeh.plotting import figure, show
+import mysql.connector
+import sqlalchemy as db
+import sys
